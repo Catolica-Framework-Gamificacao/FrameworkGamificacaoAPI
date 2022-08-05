09e17ef1a32fd245a430f398b2d776348e361d9f
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2022-08-05 13:28:24 -0300

    Auth Model

--- CtlFrameworkGamificacao/Usuario/Usuario.cs
+++ CtlFrameworkGamificacao/Usuario/Usuario.cs
@@ -2,5 +2,18 @@ namespace FrameworkGamificacaoClasses.Usuario;
 
 public class Usuario
 {
+    public int id;
+    
+    public string RA { get; set; }
+
+    public string Name { get; set; }
+
+    public bool ShowOnRanking { get; set; }
+    
+    public int Points { get; set; }
+    
+    public string Token { get; set; }
+
+    public int Role { get; set; }
     
 }
--- FrameworkGamificacaoAPI/Models/Usuario/UsuarioModel.cs
+++ FrameworkGamificacaoAPI/Models/Usuario/UsuarioModel.cs
@@ -5,7 +5,7 @@ namespace FrameworkGamificacaoAPI.Models.Usuario;
 public class UsuarioModel
 {
     public int id;
-    
+
     public string RA { get; set; }
 
     public string Name { get; set; }
@@ -15,4 +15,16 @@ public class UsuarioModel
     public int Points { get; set; }
     
     public string Token { get; set; }
+    
+    public UsuarioModel(int id, string ra, string name, bool showOnRanking, int points, string token)
+    {
+        this.id = id;
+        RA = ra;
+        Name = name;
+        ShowOnRanking = showOnRanking;
+        Points = points;
+        Token = token;
+    }
+    
+    public UsuarioModel(){}
 }
